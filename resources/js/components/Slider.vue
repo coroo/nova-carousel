82b98ee32b877928eacd7124f27844a86e1f46f3
Kuncoro Wicaksono <coroo.wicaksono@gmail.com>
2020-02-04 15:30:05 +0700

    :sparkles: Add loading animation for quote

--- resources/js/components/Slider.vue
+++ resources/js/components/Slider.vue
@@ -30,6 +30,7 @@
       <slide v-for="(item) in items" :key="item">
           <div v-for="(subItem, subKey) in item" :key="subItem">
             <div v-if="subKey.includes('quote')">
+              <svg v-if="loading" width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".5" cx="18" cy="18" r="18"/><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/></path></g></g></svg>
               <p v-if="subItem.content">{{subItem.content}}</p>
               <p v-if="subItem.author" style="margin-top:5px">~ {{subItem.author}}</p>
             </div>
@@ -63,6 +64,7 @@ export default {
         items: this.$attrs.card.card,
         customStyle: this.$attrs.card.options !== undefined && this.$attrs.card.options.customStyle !== undefined ? this.$attrs.card.options.customStyle : false,
         quotes: null,
+        loading: true,
       };
     },
     methods: {
@@ -77,7 +79,11 @@ export default {
         let duplicate = [...this.items];
         duplicate[idx].quote = getQuote;
         this.items = duplicate;
+        this.hideLoading();
       },
+      hideLoading() {
+        setTimeout(() => this.loading = false, 100);
+      }
     },
     async mounted () {
       let getIndex =[];
